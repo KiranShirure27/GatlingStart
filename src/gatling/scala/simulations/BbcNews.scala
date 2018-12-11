d521e2c9a7c1495cd5dd81ba0b5d94837ad3d5d0
SCIENCE\SHIRUREK <k.shirure@science.regn.net>
2018-12-11 11:24:57 +0000

    Removed unused imports

--- src/gatling/scala/simulations/BbcNews.scala
+++ src/gatling/scala/simulations/BbcNews.scala
@@ -2,14 +2,6 @@ package simulations
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
-
-import scala.concurrent.duration._
-
-import io.gatling.core.Predef._
-import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
-
 class BbcNews extends Simulation {
 
   val httpProtocol = http
